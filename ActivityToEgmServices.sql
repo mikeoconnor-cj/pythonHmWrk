9b7fb9f853f28449429bf038ede14945de2492e1
Michael O'Connor <michael.oconnor@ad.carejourney.com>
2021-09-02 12:57:58 -0400

    egm sql

--- ActivityToEgmServices.sql
+++ ActivityToEgmServices.sql
@@ -1,24 +1,39 @@
 --EGM Services
-SELECT FK_PATIENT_ID 
-	, ACTIVITY_TYPE_CD 
-	, FACILITY_REVENUE_CENTER_CD 
+SELECT SPLIT_PART(PK_ACTIVITY_ID,'|', 3) AS clm_id 
+	, SPLIT_PART(PK_ACTIVITY_ID,'|', 4) AS line_num
+	, FK_PATIENT_ID AS bene_key ---for Change, there's a hash for a2024 there's an org_src_id prefix
+	--not an accurate regex for body modifier yet
+	, CASE 
+		WHEN regexp_like(PROCEDURE_HCPCS_MOD_CD_LIST[0],'[A-Z]{2}') THEN PROCEDURE_HCPCS_MOD_CD_LIST[0]
+		WHEN regexp_like(PROCEDURE_HCPCS_MOD_CD_LIST[1],'[A-Z]{2}') THEN PROCEDURE_HCPCS_MOD_CD_LIST[1]
+		WHEN regexp_like(PROCEDURE_HCPCS_MOD_CD_LIST[2],'[A-Z]{2}') THEN PROCEDURE_HCPCS_MOD_CD_LIST[2]
+	  END AS body_mod
+	, PROCEDURE_HCPCS_MOD_CD_LIST[0] AS mod_1
+	, PROCEDURE_HCPCS_MOD_CD_LIST[1] AS mod_2
+	, claim_line_allowed_amt AS pay
+	, FACILITY_PLACE_OF_SERVICE_CD AS pos_cd
+	, FACILITY_NPI_NUM AS prf_at_grp_npi
+	
+	, FACILITY_REVENUE_CENTER_CD AS rev_cd
+	
+	
 	, FK_PROCEDURE_ID 
 	, PROCEDURE_HCPCS_CD 
-	, PROCEDURE_HCPCS_MOD_CD_LIST 
-	, PROCEDURE_HCPCS_MOD_CD_LIST[0] AS mod0
-	, PROCEDURE_HCPCS_MOD_CD_LIST[1] AS mod1
-	, PROCEDURE_HCPCS_MOD_CD_LIST[2] AS mod2
+
+	, PROCEDURE_HCPCS_MOD_CD_LIST[2] AS mod3
+	, PROCEDURE_HCPCS_MOD_CD_LIST 	
 	, PROCEDURE_ICD_9_CD 
 	, PROCEDURE_ICD_10_CD 
 	, MEDICATION_NDC_SPL_CD	
 	, MEDICATION_HCPCS_CD
 	, DME_HCPCS_CD
+	, claim_type_cd
 	, CASE 
 		WHEN claim_type_cd = '40' THEN 'op'
 		WHEN claim_type_cd IN ('70','71') THEN 'pb'
 		WHEN claim_type_cd IN ('60', '61') THEN 'ip'
 		WHEN claim_type_cd = '10' THEN 'hh'
-		-- other cases
+		-- other cases '01' '02' is med
 	  END AS src_file	
 	, CASE 
 		WHEN ACTIVITY_TYPE_CD = 'fac_proc'
@@ -29,4 +44,6 @@ SELECT FK_PATIENT_ID
 			THEN DME_HCPCS_CD
 		ELSE PROCEDURE_HCPCS_CD 			
 	  END AS src_code
+	  , ACTIVITY_TYPE_CD 
+	  , PK_ACTIVITY_ID 
 FROM insights.ACTIVITY 
